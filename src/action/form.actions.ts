c8167fb46634f9f2fc8442edabc7cf92d34515cc
Aakash <aakashanand1995@gmail.com>
2024-06-01 16:15:17 +0530

    second commit

--- src/action/form.actions.ts
+++ src/action/form.actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { ZodError, z } from "zod";
+import { ZodError, ZodIssueCode, z } from "zod";
 
 export async function submitAction(formData: FormData) {
   let username = formData.get("username");
@@ -10,10 +10,5 @@ export async function submitAction(formData: FormData) {
     username: z.string(),
     password: z.string(),
   });
-
-  if (user.parse({ username, password })) {
-    throw new ZodError();
-  }
-
   console.log(user.parse({ username, password }));
 }
